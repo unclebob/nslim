e0f3e4449601bb095120c04bd8ab697b11dceaa3
jediwhale <mikes@.(none)>
2009-01-12 14:44:43 -0800

    add simple type conveter base class

---
+++ source/mtee/Operators/Converter.cs
@@ -0,0 +1,20 @@
+﻿using fitnesse.mtee.engine;
+
+namespace fitnesse.mtee.Operators {
+    public abstract class Converter<T>: ParseOperator, ComposeOperator {
+        public bool IsMatch(Processor processor, State state) {
+            return state.Type == typeof(T);
+        }
+
+        public object Compose(Processor processor, State state) {
+            return Compose((T)state.Instance);
+        }
+
+        public object Parse(Processor processor, State state) {
+            return Parse(state.ParameterValueString);
+        }
+
+        protected abstract T Parse(string input);
+        protected abstract string Compose(T input);
+    }
+}
---
+++ source/unitTest/operators/ConverterTest.cs
@@ -0,0 +1,42 @@
+﻿using fitnesse.mtee.engine;
+using fitnesse.mtee.model;
+using fitnesse.mtee.Operators;
+using NUnit.Framework;
+
+namespace fitnesse.unitTest.operators {
+    [TestFixture] public class ConverterTest {
+        private readonly Processor processor = new Processor(new Assemblies());
+
+        [Test] public void CustomTypeIsParsed() {
+            var converter = new CustomConverter();
+            var state = new State(typeof(CustomClass), new TreeLeaf<object>("info"));
+            Assert.IsTrue(converter.IsMatch(processor, state));
+            var result = converter.Parse(processor, state) as CustomClass;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("custominfo", result.Info);
+        }
+
+        [Test] public void CustomTypeIsComposed() {
+            var converter = new CustomConverter();
+            var state = new State(new CustomClass {Info = "stuff"}, typeof(CustomClass));
+            Assert.IsTrue(converter.IsMatch(processor, state));
+            var result = converter.Compose(processor, state) as string;
+            Assert.IsNotNull(result);
+            Assert.AreEqual("mystuff", result);
+        }
+
+        private class CustomConverter: Converter<CustomClass> {
+            protected override CustomClass Parse(string input) {
+                return new CustomClass {Info = ("custom" + input)};
+            }
+
+            protected override string Compose(CustomClass input) {
+                return "my" + input.Info;
+            }
+        }
+
+        private class CustomClass {
+            public string Info;
+        }
+    }
+}
